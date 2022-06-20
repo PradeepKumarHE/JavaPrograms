4427da7a2a7a3f4d28d1199ddee34c0bf02e3ec1
Pradeep Kumar <pradeepkumarhe1989@gmail.com>
2022-06-20 12:16:42 +0530

    Update Sum4Index.java

--- src/main/java/com/pradeep/programs/interview_programs/Sum4Index.java
+++ src/main/java/com/pradeep/programs/interview_programs/Sum4Index.java
@@ -1,10 +1,8 @@
 package com.pradeep.programs.interview_programs;
 
 public class Sum4Index {
-
-	public static void main(String[] args) {
-		
-		int [] arr 	= {2,4,6,1,9,8,3,6};
+	public static void main(String[] args) {		
+		int [] arr = {2,4,6,1,9,8,3,6};
 		for(int i=0;i<arr.length;i++) {
 			for(int j=i+1;j<arr.length;j++) {
 				if(arr[i]+arr[j]==4) {
@@ -12,7 +10,5 @@ public class Sum4Index {
 				}
 			}	
 		}
-
 	}
-
 }
