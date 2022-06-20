ae680d134a378fd700d3b60a2db702718203da31
Pradeep Kumar <pradeepkumarhe1989@gmail.com>
2022-06-20 12:17:27 +0530

    Update FirstNonRepeatingCharacter.java

--- src/main/java/com/pradeep/programs/interview_programs/FirstNonRepeatingCharacter.java
+++ src/main/java/com/pradeep/programs/interview_programs/FirstNonRepeatingCharacter.java
@@ -3,9 +3,9 @@ package com.pradeep.programs.interview_programs;
 public class FirstNonRepeatingCharacter {
 	public static void main(String[] args) {
 		String inputStr = "teeter";
-		for (char i : inputStr.toCharArray()) {
-			if (inputStr.indexOf(i) == inputStr.lastIndexOf(i)) {
-				System.out.println("First non-repeating character is: " + i);
+		for (char c : inputStr.toCharArray()) {
+			if (inputStr.indexOf(c) == inputStr.lastIndexOf(c)) {
+				System.out.println("First non-repeating character is: " + c);
 				break;
 			}
 		}
