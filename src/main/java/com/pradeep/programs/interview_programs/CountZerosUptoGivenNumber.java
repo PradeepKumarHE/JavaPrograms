38293b21c3733902b5337045131272c08d213768
Pradeep Kumar <pradeepkumarhe1989@gmail.com>
2024-10-09 17:11:29 +0530

    Create CountZerosUptoGivenNumber.java

---
+++ src/main/java/com/pradeep/programs/interview_programs/CountZerosUptoGivenNumber.java
@@ -0,0 +1,16 @@
+package com.pradeep.programs.interview_programs;
+
+public class CountZerosUptoGivenNumber {
+	public static void main(String[] args) {		
+	    int num=45 , count=1;
+      if(num==0){
+        System.out.println("Count of zeros is :: "+count);
+      }else{
+        while(num / 10 != 0){
+          count=count + num / 10;
+          num = num / 10;
+        }
+      }  
+     System.out.println("Count of zeros is :: "+count);
+	}
+}
