a70518c41326e3af0ad539522c70dc5c4c848bf3
Pradeep Kumar <pradeepkumarhe1989@gmail.com>
2024-10-09 17:03:18 +0530

    Update RempveDuplicateLetterInString.java

--- src/main/java/com/pradeep/programs/java8programs/RempveDuplicateLetterInString.java
+++ src/main/java/com/pradeep/programs/java8programs/RempveDuplicateLetterInString.java
@@ -4,8 +4,17 @@ import java.util.stream.Collectors;
 
 
 public class RempveDuplicateLetterInString {
-	public static void main(String args[]) {
+  public static void main(String args[]) {
     String str="pradeep";
     str.chars().mapToObj(obj -> (char) obj).collect(Collectors.toSet()).forEach(System.out::print);
+    /* java 7*/
+	  String output="";
+	  char [] chars=str.toCharArray();
+	  for(char ch:chars){
+	     if(!output.contains(String.valueOf(ch))){
+		     output=output+ch;
+	     }
+	  }
+	  System.out.println(output);
   }
 }
